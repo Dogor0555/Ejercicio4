c7f85e24b3bc8cf455783d544dd1e1cd76920cc6
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2025-02-15 05:35:11 -0600

    Ejercicio 4

--- Ejercicio4/Program.cs
+++ Ejercicio4/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+bool esMayorDeEdad = true;  // Variable booleana que indica si la persona es mayor de edad o no.
+
+if (esMayorDeEdad)
+{
+    Console.WriteLine("Eres mayor de edad.");
+}
+else
+{
+    Console.WriteLine("Eres menor de edad.");
+}
+
+Console.WriteLine("El Ing Daro es muy feo.");
